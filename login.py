bf41839b3059f9c8916ce870e9e44f9a4ed0ae7e
Joanna Nelson <nels.joanna@gmail.com>
2018-05-25 02:10:10 -0400

    Update login.py

--- login.py
+++ login.py
@@ -7,12 +7,10 @@ from getpass import getpass
 
 usr = input('Enter your username or email: ')
 pwd = getpass('Enter your password : ')
-url = 'https://www.twitter.com/login'
-
 
 def logintwitter():
     browser = selenium.webdriver.Chrome('/Users/Joanna/Exec/chromedriver')
-    browser.get(url)
+    browser.get('https://www.twitter.com/login')
     fillusr = browser.find_element_by_xpath('//*[@id="page-container"]/div/div[1]/form/fieldset/div[1]/input')
     fillusr.send_keys(usr)
     fillpass = WebDriverWait(browser, 10).until(
@@ -20,6 +18,18 @@ def logintwitter():
     fillpass.send_keys(pwd)
     login = browser.find_element_by_xpath('//*[@id="page-container"]/div/div[1]/form/div[2]/button')
     login.click()
+    return browser
 
+browser = logintwitter()
 
-logintwitter()
+def PoshmarkLogin():
+	browser.get('https://www.poshmark.com/login')
+	poshmarkuser = browser.find_element_by_xpath('//*[@id="login_form_username_email"]')
+	poshmarkuser.send_keys(usr)
+	fillpass = browser.find_element_by_xpath('//input[@id="login_form_password"]')
+	fillpass.send_keys(pwd)
+	loginbutton = browser.find_element_by_xpath('//button[.="Log In"]')
+	loginbutton.click()
+	return browser
+
+browser = PoshmarkLogin()
